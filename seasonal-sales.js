f29b231d0acacaca29be295b3132ecb0b237c54c
ellisthomas <ellisthomas@gmail.com>
2017-03-14 12:35:38 -0500

    printing to dom for both json files added css to break up sections

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -3,7 +3,7 @@
 var productContainer = document.getElementById("productContainer");
 
 function makeDom(xhrData) {
-	var productString =""
+	var productString = ""
 	var currentProduct;
 	for (var i = 0; i < xhrData.products.length; i++) {
 		currentProduct = xhrData.products[i];
@@ -16,7 +16,7 @@ function makeDom(xhrData) {
 	productString += `<p>${currentProduct.category_id}</p>`;
 	productString += `</div></div>`;
   
-}
+	}
 		productContainer.innerHTML = productString;
 }
 
@@ -29,7 +29,6 @@ function executeThisCodeAfterFileFails() {
 	console.log("boooooo");
 }
 
-
 var myRequest = new XMLHttpRequest();
 myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
@@ -37,53 +36,41 @@ myRequest.open("GET", "products.json");
 myRequest.send();
 
 
-// var productContainer = document.getElementById("productContainer");
-// var productString = ""
-// var currentProduct;
-
-// function makeDom(xhrData){
-
-// 	for (var i = 0; i < xhrData.products.length; i++){
-// 		currentProduct = xhrData.products[i];
-// // console.log("products", xhrData.products);
-// 		productString += `<div class="col-md-6 col-md-4">`;
-// 		productString += `<div class="thumbnail">`;
-// 		productString += `<p>${products.id}</p>`;
-// 		productString += `<p>${currentProduct.name}</p>`;
-// 		productString += `<p>${currentProduct.price}</p>`;
-// 		productString += `<p>${currentProduct.category_id}</p>`;
-// 		productString += `<div></div>`;
-
-// 	}
-
-// 	productContainer.innerHTML = productString;
-// }
-
-// function executeAfterProductFileLoaded(){
-// 	var data = JSON.parse(this.responseText);
-// 	makeDom(data);
-// }
-
-// function executeAfterProductFileFails(){
-// 	alert("Error Code Failed");
-// }
-
-// var myRequest = new XMLHttpRequest();
-// myRequest.addEventListener("load", executeAfterProductFileLoaded);
-// myRequest.addEventListener("error", executeAfterProductFileFails);
-// myRequest.open("GET", "products.json");
-// myRequest.send();
-
 //////////////////////////// CATEGORIES ////////////////////////////////////////////
-// var categoriesContainer = document.getElementById("categoriesContainer");
 
-// function categoryToDom(){
-// 	var categoryString = "";
-// 	var categories;
-// }
+var categoryContainer = document.getElementById("categoryContainer");
 
-// var myRequestCategory = new XMLHttpRequest();
-// myRequestCategory.addEventListener("load", executeTheCategoryAfterFileLoaded);
-// myRequestCategory.addEventListener("error", executeTheCategoryAlertAfterFileFails);
-// myRequestCategory.open("GET", "categories.json");
-// myRequestCategory.send();
+function categoryDom(xhrData) {
+	var categoryString = ""
+	var currentCategory;
+	for (var j = 0; j < xhrData.categories.length; j++) {
+		currentCategory = xhrData.categories[j];
+
+	categoryString += `<div class="col-sm-6 col-md-4">`;
+	categoryString += `<div class="thumbnail">`;
+	categoryString += `<p>${currentCategory.id}</p>`;
+	categoryString += `<p>${currentCategory.name}</p>`;
+	categoryString += `<p>${currentCategory.season}</p>`;
+	categoryString += `<p>${currentCategory.discount}</p>`;
+	categoryString += `</div></div>`;
+  
+	}
+		categoryContainer.innerHTML = categoryString;
+}
+
+function executeCategoryCodeAfterFileLoaded() {
+	var data = JSON.parse(this.responseText);
+	categoryDom(data);
+	console.log("data", data);
+}
+
+function executeCategoryCodeAfterFileFails() {
+	console.log("boooooo");
+}
+
+
+var myRequestCategory = new XMLHttpRequest();
+myRequestCategory.addEventListener("load", executeCategoryCodeAfterFileLoaded);
+myRequestCategory.addEventListener("error", executeCategoryCodeAfterFileFails);
+myRequestCategory.open("GET", "categories.json");
+myRequestCategory.send();
